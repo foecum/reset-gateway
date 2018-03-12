fc225fcd356a7621d488ca90ebd65fbd22143e0a
Fauz O. Ecum <fauz.ecum@foecum.com>
2018-03-12 21:30:54 +0200

    Removed unneccesaru logs

--- add-route.go
+++ add-route.go
@@ -18,13 +18,10 @@ func main() {
 			if strings.Compare(networkErr, strings.Trim(string(b), "\n")) == 0 {
 				log.Printf("Network gateway not set. Setting...\n")
 				resetGW := exec.Command("sudo", "ip", "route", "add", "default", "via", "192.168.8.1")
-				b, err := resetGW.CombinedOutput()
+				_, err := resetGW.CombinedOutput()
 
 				if err != nil {
-					log.Printf("%v\n", err)
-					log.Printf("%v\n", string(b))
 					exitChan <- "Exiting loop"
-
 					return
 				}
 				log.Printf("Network gateway set\n")
